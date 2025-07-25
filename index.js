413f208e9c42b8065648fa5dafd395cae72e3ed7
fzd <fzddnp@hotmail.com>
2025-07-25 09:21:13 +0330

    Add UI for app

--- index.js
+++ index.js
@@ -17,41 +17,73 @@ function getComputerChoice() {
   return choice;
 }
 
-function getHumanChoice() {
-  return prompt("What's your choice? Rock, Paper, Scissors").toLowerCase();
-}
+// function getHumanChoice() {
+//   return prompt("What's your choice? Rock, Paper, Scissors").toLowerCase();
+// }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === "rock" && computerChoice === "paper") {
+function playRound(usr, com) {
+  const result = document.querySelector("#result");
+
+  if (usr === "rock" && com === "paper") {
     computerScore += 1;
-    console.log("You lose! Paper beats Rock");
-  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    // console.log("You lose! Paper beats Rock");
+    result.textContent = "You lose! Paper beats Rock";
+    result.style.color = "red";
+    result.style.fontWeight = "bold";
+  } else if (usr === "paper" && com === "scissors") {
     computerScore += 1;
-    console.log("You lose! Scissors beats Paper");
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    // console.log("You lose! Scissors beats Paper");
+    result.textContent = "You lose! Scissors beats Paper";
+    result.style.color = "red";
+    result.style.fontWeight = "bold";
+  } else if (usr === "scissors" && com === "rock") {
     computerScore += 1;
-    console.log("You lose! Rock beats Scissors");
-  } else if (humanChoice === computerChoice) {
-    console.log("Draw!");
+    // console.log("You lose! Rock beats Scissors");
+    result.textContent = "You lose! Rock beats Scissors";
+    result.style.color = "red";
+    result.style.fontWeight = "bold";
+  } else if (usr === com) {
+    // console.log("Draw!");
+    result.textContent = "Draw";
+    result.style.color = "cyan";
+    result.style.fontWeight = "bold";
   } else {
     humanScore += 1;
-    console.log("You won the round");
+    // console.log("You won the round");
+    result.textContent = "You won the round";
+    result.style.color = "lime";
+    result.style.fontWeight = "bold";
   }
 }
 
 function playGame() {
-  while (humanScore < 3 && computerScore < 3) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  if (humanScore < 3 && computerScore < 3) {
+    playRound(userChoice, getComputerChoice());
 
-    playRound(humanSelection, computerSelection);
+    let score = document.querySelector("#score");
+    score.textContent = `You: ${humanScore} vs PC: ${computerScore}`;
+    score.style.fontWeight = "bold";
   }
-
   if (humanScore === 3) {
-    console.log("You Won The Game!");
+    // console.log("You Won The Game!");
+    result.textContent = "You Won The Game!";
+    result.style.color = "lime";
+    result.style.fontWeight = "bold";
+    result.style.fontSize = "22px";
   } else if (computerScore === 3) {
-    console.log("Game Over");
+    // console.log("Game Over");
+    result.textContent = "Game Over";
+    result.style.color = "red";
+    result.style.fontWeight = "bold";
+    result.style.fontSize = "22px";
   }
 }
 
-playGame();
+let userChoice = "";
+
+const buttons = document.querySelector("#buttons");
+
+buttons.addEventListener("click", (e) => {
+  userChoice = e.target.textContent.toLowerCase();
+  playGame();
+});
